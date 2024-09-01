684347e700ded1a2a5c699c20f2042b7aa306c8a
anuragrege <anuragrege@gmail.com>
2024-09-01 23:18:13 +0530

    style options page

--- face-autthentication/src/OptionsPage.js
+++ face-autthentication/src/OptionsPage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import BackgroundMonitor from "./BackgroundMonitor";
-import "./App.css";
+import "./options.css";
 
 function OptionsPage() {
   const navigate = useNavigate();
   const location = useLocation();
   const { userData } = location.state || {};
-  const encoding = userData ? new Float32Array(JSON.parse(userData.encoding)) : null;
+  const encoding = userData
+    ? new Float32Array(JSON.parse(userData.encoding))
+    : null;
 
   const handleGazeTest = () => {
     navigate("/gaze-mouse");
@@ -19,32 +21,38 @@ function OptionsPage() {
 
   const handleMonitoringFailure = () => {
     console.log("Background monitoring failed.");
-    
+
     // Notify the user
-    alert("Background monitoring failed. Please make sure you are in front of the camera and try again.");
-    
+    alert(
+      "Background monitoring failed. Please make sure you are in front of the camera and try again."
+    );
+
     // Optionally redirect to a different page or retry the authentication
     window.location.href = "/face-authentication"; // Redirect to the face authentication page
     // OR
     // setAuthenticationMessage("Background monitoring failed. Please try again."); // Show a message in the UI
-  
+
     // Optionally log the failure (e.g., to an analytics service or server)
     // axios.post('/api/log', { error: "Background monitoring failed" });
   };
-  
 
   return (
     <div className="App">
-      <h1>Select a Test</h1>
-      <button className="Option-button" onClick={handleGazeTest}>
-        Gaze Test
-      </button>
-      <button className="Option-button" onClick={handleLeftRightTest}>
-        Left Right Test
-      </button>
-      {encoding && (
-        <BackgroundMonitor encoding={encoding} onFail={handleMonitoringFailure} />
-      )}
+      <div className="Options-container">
+        <h4 className="ab">Select a Test</h4>
+        <button className="Option-button" onClick={handleGazeTest}>
+          Gaze Test
+        </button>
+        <button className="Option-button" onClick={handleLeftRightTest}>
+          Left Right Test
+        </button>
+        {encoding && (
+          <BackgroundMonitor
+            encoding={encoding}
+            onFail={handleMonitoringFailure}
+          />
+        )}
+      </div>
     </div>
   );
 }
